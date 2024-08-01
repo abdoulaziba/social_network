8b716ea704852bc56cbb51a13221f24488df0570
abdoulaziba <baabdoulba770@gmail.com>
2024-08-01 17:47:13 +0000

    feat: handling authentification

--- frontend/social-network/src/app/page.js
+++ frontend/social-network/src/app/page.js
@@ -3,93 +3,71 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <div className="wrapper">   
+        <section className="sign-in-page">
+            <div id="container-inside">
+                <div id="circle-small"></div>
+                <div id="circle-medium"></div>
+                <div id="circle-large"></div>
+                <div id="circle-xlarge"></div>
+                <div id="circle-xxlarge"></div>
+            </div>
+            <div className="container p-0">
+                <div className="row no-gutters">
+                    <div className="col-md-6 text-center pt-5">
+                        <div class="sign-in-detail text-white">
+                            <a class="sign-in-logo mb-5" href="#"><img src="/images/logo-full.png" class="img-fluid" alt="logo"/></a>
+                            <div class="sign-slider overflow-hidden ">
+                                <ul  class="swiper-wrapper list-inline m-0 p-0 ">
+                                    <li class="swiper-slide">
+                                        <img src="/images/login/1.png" class="img-fluid mb-4" alt="logo"/>
+                                        <h4 class="mb-1 text-white">Find new friends</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                                    </li>
+                                    <li class="swiper-slide">
+                                        <img src="/images/login/2.png" class="img-fluid mb-4" alt="logo"/> 
+                                        <h4 class="mb-1 text-white">Connect with the world</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                                    </li>
+                                    <li class="swiper-slide">
+                                        <img src="/images/login/3.png" class="img-fluid mb-4" alt="logo"/>
+                                        <h4 class="mb-1 text-white">Create new events</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 bg-white pt-5 pb-lg-0 pb-5">
+                        <div className="sign-in-from">
+                            <h1 className="mb-0">Sign in</h1>
+                            <p>Enter your email address and password to access admin panel.</p>
+                            <form className="mt-4">
+                                <div class="form-group">
+                                        <label class="form-label" for="exampleInputEmail1">Email address</label>
+                                        <input type="email" class="form-control mb-0" id="exampleInputEmail1" placeholder="Enter email or username"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="exampleInputPassword1">Password</label>
+                                        <a href="#" class="float-end">Forgot password?</a>
+                                        <input type="password" class="form-control mb-0" id="exampleInputPassword1" placeholder="password"/>
+                                    </div>
+                                    <div class="w-90">
+                                        {/* <div class="form-check d-inline-block mt-2 pt-1">
+                                            <input type="checkbox" class="form-check-input" id="customCheck11"/>
+                                            <label class="form-check-label" for="customCheck11">Remember Me</label>
+                                        </div> */}
+                                        <button type="submit" class="btn btn-primary float-end">Sign in</button>
+                                        <span class="dark-color d-inline-block line-height-2">Don't have an account? <a href="/register">Sign up</a></span>
+                                    </div>
+                                  <div class="sign-info">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
   );
 }
